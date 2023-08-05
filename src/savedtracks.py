2670f7fdae86c775dde530d3a93f6cbd5588f01d
micheledibi <micheledibi2002@gmail.com>
2023-08-05 17:23:13 +0200

    Introdotto salvataggio brani di playlist

--- src/savedtracks.py
+++ src/savedtracks.py
@@ -4,6 +4,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import numpy as np
 from datetime import date
 
+import saveTracks
+
 client_id = "1572e8f11a55483ba6336cc98058160e"
 client_secret = "f6e304f95c1d41d1b3f9434437e88435"
 redirect_uri = "http://localhost:8888/callback"
@@ -12,13 +14,13 @@ auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret,
                             redirect_uri=redirect_uri, scope='user-library-read')
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-offset = 0
-limit = 50
-
-saved_tracks = []
 artists = []
 albums = []
 
+offset = 0
+limit = 50
+brani_preferiti = []
+"""
 while True:
     results = sp.current_user_saved_tracks(limit=limit, offset=offset)
     tracks = results['items']
@@ -26,109 +28,29 @@ while True:
     if not tracks:
         break
 
-    for track in tracks:
-        added_at = track['added_at']
-        track = track['track']
-        features = sp.audio_features(track['id'])
-        
-        if features[0] != None:
-            
-            print(track['name'])
-
-            artist_result = sp.artist(track['artists'][0]['id'])
-            current_artist = {
-                'artist_id': track['artists'][0]['id'],
-                'artist_name': track['artists'][0]['name'],
-                'artist_followers': artist_result['followers']['total'],
-                'artist_genres': artist_result['genres'],
-                'artist_popularity': artist_result['popularity']
-            }            
-            artists.append(current_artist)
-
-            side_artists = []
-            for side_artist in track['artists'][1:]:
-                
-                artist_result = sp.artist(side_artist['id'])
-                
-                current_artist = {
-                    'artist_id': side_artist['id'],
-                    'artist_name': side_artist['name'],
-                    'artist_followers': artist_result['followers']['total'],
-                    'artist_genres': artist_result['genres'],
-                    'artist_popularity': artist_result['popularity']
-                }
-                
-                side_artists.append(side_artist['id'])
-            
-            album_result = sp.album(track['album']['id'])
-            
-            current_album = {
-                'album_id': track['album']['id'],
-                'album_name': track['album']['name'],
-                'album_total_tracks': track['album']['total_tracks'],
-                'album_type': track['album']['album_type'],
-                'album_release_date': track['album']['release_date'],
-                # 'album_genres': album_result['genres'],
-                'album_label': album_result['label'],
-                'album_popularity': album_result['popularity']
-            }    
-            
-            albums.append(current_album)
-            
-            """
-            current_track = {
-                'id': track['id'],
-                'added_at': added_at,
-                'name': track['name'],
-                'album': track['album']['name'],
-                'release_date': track['album']['release_date'],
-                'main_artist': track['artists'][0]['name'], 
-                'side_artists': side_artists,
-                'popularity': track['popularity'],
-                'key': features[0]['key'],
-                'bpm': round(features[0]['tempo']),
-                'energy': round(features[0]['energy'], 2),
-                'danceability': round(features[0]['danceability'], 2),
-                'happiness': round(features[0]['valence'], 2),
-                'loudness': round(features[0]['loudness'], 2),
-                'acousticness': round(features[0]['acousticness'], 2),
-                'instrumentalness': round(features[0]['instrumentalness'], 2),
-                'liveness': round(features[0]['liveness'], 2),
-                'mode': features[0]['mode'],
-                'speechiness': round(features[0]['speechiness'], 3),
-                'time_signature': features[0]['time_signature']
-            }
-            """
-            current_track = {
-                'track_id': track['id'],
-                'track_added_at': added_at,
-                'track_name': track['name'],
-                'album_id': track['album']['id'],
-                'main_artist_id': track['artists'][0]['id'], 
-                'side_artists_id': side_artists,
-                'track_duration': track['duration_ms'],
-                'track_explicit': track['explicit'],
-                'track_popularity': track['popularity'],
-                'track_key': features[0]['key'],
-                'track_bpm': features[0]['tempo'],
-                'track_energy': features[0]['energy'],
-                'track_danceability': features[0]['danceability'],
-                'track_happiness': features[0]['valence'],
-                'track_loudness': features[0]['loudness'],
-                'track_acousticness': features[0]['acousticness'],
-                'track_instrumentalness': features[0]['instrumentalness'],
-                'track_liveness': features[0]['liveness'],
-                'track_mode': features[0]['mode'],
-                'track_speechiness': features[0]['speechiness'],
-                'track_time_signature': features[0]['time_signature']
-            }
-            
-            saved_tracks.append(current_track)
-        
+    brani_preferiti.extend(tracks)
     offset += limit
 
-df_saved_tracks = pd.DataFrame(saved_tracks)
-df_saved_tracks.to_csv(f"saved_tracks_{date.today()}.csv", index=False)
+df_brani_preferiti, artists, albums = saveTracks.saveTracksIntoFile(sp, brani_preferiti, "brani_preferiti", artists, albums)
+"""
+frames = []
+playlist_IDs = [
+    "37i9dQZF1DWVKDF4ycOESi", # New Music Friday Italy
+    "37i9dQZF1DX6wfQutivYYr", # Hot Hits Italia
+    "37i9dQZF1DX0ckkFHFOpuK", # in tendenza
+    "37i9dQZEVXbIQnj7RRhdSX", # Top 50 Italia
+    "37i9dQZF1DX4JAvHpjipBk", # New Music Friday
+    ]
+
+for playlist_id in playlist_IDs:
+    results = sp.playlist(playlist_id=playlist_id)
+    tracks = results['tracks']['items']
+    temp_dataframe, artist, albums = saveTracks.saveTracksIntoFile(sp, tracks, results['name'], artists, albums, saveCSV=False)
+    frames.append(temp_dataframe)
+
+other_tracks = pd.concat(frames)
+other_tracks = other_tracks.drop_duplicates(subset="track_id")
+other_tracks.to_csv(f"brani_scaricati_{date.today()}.csv", index=False)
 
 df_albums = pd.DataFrame(albums)
 df_albums = df_albums.drop_duplicates(subset="album_id")
