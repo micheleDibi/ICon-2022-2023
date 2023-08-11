877c4f756c9aed0ffcf9a5e561956f38b8de1895
micheledibi <micheledibi2002@gmail.com>
2023-08-11 19:15:09 +0200

    terminato clustering con valutazione

--- src/newMain.py
+++ src/newMain.py
@@ -1,75 +1,126 @@
+import ast
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from os.path import exists
 
-import seaborn as sns
-from sklearn.preprocessing import MultiLabelBinarizer
 from datetime import date
-from sklearn.decomposition import PCA
-
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
-import matplotlib.pyplot as plt
-
-import ast
-
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import MultiLabelBinarizer
 from mapping_genres import map_genre_to_macro_category
 
-path_favorites_tracks = "./datasets/brani_preferiti_2023-08-06.csv"
-path_saved_tracks = "./datasets/brani_scaricati_2023-08-06.csv"
-path_artists = "./datasets/artists_2023-08-06.csv"
+path_artists = "./datasets/artists_2023-08-11.csv"
 
-def best_k_clustering(dataframe, min_value = 2, max_value = 11):
-    k_values = range(min_value, max_value)
+def get_metrics(eps, min_samples, dataset, iter_):    
+    dbscan_model_ = DBSCAN( eps = eps, min_samples = min_samples)
+    dbscan_model_.fit(dataset)
     
-    elbow_scores = []
-    silhouette_scores = []
+    noise_indices = dbscan_model_.labels_ == -1
     
-    for k in k_values:
-        kmeans = KMeans(n_clusters=k, n_init='auto')
-        kmeans.fit(dataframe)
+    if True in noise_indices:
+        neighboors = NearestNeighbors(n_neighbors = 6).fit(dataset)
+        distances, indices = neighboors.kneighbors(dataset)
+        noise_distances = distances[noise_indices, 1:]
+        noise_mean_distance = round(noise_distances.mean(), 3)
+    else:
+        noise_mean_distance = None
         
-        elbow_scores.append(kmeans.inertia_)
-        silhouette_scores.append(silhouette_score(dataframe, kmeans.labels_))
+    number_of_clusters = len(set(dbscan_model_.labels_[dbscan_model_.labels_ >= 0]))
         
-    print(elbow_scores)        
-    plt.figure(figsize=(10, 6))
-    plt.plot(k_values, elbow_scores, marker='o')
-    plt.xlabel("Numero di cluster (K)")
-    plt.ylabel("Somma dei quadrati delle distanze")
-    plt.title("Analisi del gomito")
-    plt.savefig("./img/analisi_del_gomito.png")
-    
-    plt.figure(figsize=(10, 6))
-    plt.plot(k_values, silhouette_scores, marker='o')
-    plt.xlabel("Numero di cluster (k)")
-    plt.ylabel("Coef. di silhouette")
-    plt.title("Coefficienti di silhouette")
-    plt.savefig("./img/coeff_silhouette.png")
-    
-def clustering(dataframe, columns, k):
-    kmeans_model = KMeans(n_clusters=k, n_init=10, init='k-means++')
-    kmeans_model.fit(dataframe[columns])
-    
-    # centroids = kmeans_model.cluster_centers_
-    
-    dataframe["cluster"] = kmeans_model.labels_
-    print(dataframe["cluster"].value_counts()) 
+    print("%3d | Tested with eps = %3s and min_samples = %3s | %5s %4s" % (iter_, eps, min_samples, str(noise_mean_distance), number_of_clusters))
+        
+    return(noise_mean_distance, number_of_clusters)
 
-    dataframe.to_csv(f"scaled_dataframe_clustering_{date.today()}.csv")
+def ricerca_iperparametri(dataframe):
+    eps_to_test = [round(esp, 1) for esp in np.arange(0.1, 2, 0.1)]
+    min_samples_to_test = range(5, 50, 5)
     
+    # Dataframe per la metrica sulla distanza media dei noise points dai K punti più vicini
+    results_noise = pd.DataFrame(
+        data=np.zeros((len(eps_to_test), len(min_samples_to_test))),
+        columns=min_samples_to_test,
+        index=eps_to_test
+    )
+    
+    # Dataframe per la metrica sul numero di cluster
+    results_cluster = pd.DataFrame(
+        data=np.zeros((len(eps_to_test), len(min_samples_to_test))),
+        columns=min_samples_to_test,
+        index=eps_to_test
+    )
+    
+    iter_ = 0
+    print("ITER| INFO%s |  DIST    CLUS" % (" "*39))
+    print("-"*65)
+    
+    for eps in eps_to_test:
+        for min_samples in min_samples_to_test:
+            
+            iter_ += 1
+            
+            noise_metric, cluster_metric, = get_metrics(eps, min_samples, dataframe, iter_)
+            
+            results_noise.loc[eps, min_samples] = noise_metric
+            results_cluster.loc[eps, min_samples] = cluster_metric 
+            
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8) )
 
-if exists(path_saved_tracks) and exists(path_favorites_tracks) and exists(path_artists):
-    """
-    df_favorites_tracks = pd.read_csv(path_favorites_tracks)
-    df_saved_tracks = pd.read_csv(path_saved_tracks)
-    df_favorites_tracks["liked"] = True
-    df_saved_tracks["liked"] = False
-    df_tracks = pd.concat([df_favorites_tracks, df_saved_tracks])
-    """
+    sns.heatmap(results_noise, annot = True, ax = ax1, cbar = False).set_title("METRIC: Mean Noise Points Distance")
+    sns.heatmap(results_cluster, annot = True, ax = ax2, cbar = False).set_title("METRIC: Number of clusters")
+
+    ax1.set_xlabel("N"); 
+    ax2.set_xlabel("N")
+    ax1.set_ylabel("EPSILON"); 
+    ax2.set_ylabel("EPSILON")
+
+    plt.tight_layout(); 
+    plt.savefig("parameters.png")
     
+def clustering(dataframe, eps=0.3, min_samples=5):
+    
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    
+    labels = dbscan.fit_predict(dataframe)
+    dataframe.loc[:, 'cluster'] = labels
+        
+    return dataframe
+
+"""
+def best_parameters_clustering(data):
+    best_eps = None
+    best_min_samples = None
+    best_score = -1
+    
+    for eps in np.arange(0.1, 1.0, 0.1):
+        for min_samples in range(1, 10):
+            dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+            labels = dbscan.fit_predict(data)
+            
+            if len(np.unique(labels)) > 1:
+                score = silhouette_score(data, labels)
+                
+                # print(f"eps: {eps} - min_samples: {min_samples} - score: {score}")
+                
+                if score > best_score:
+                    best_score = score
+                    best_eps = eps
+                    best_min_samples = min_samples
+                    
+    print(f"Best Silhouette Score: {best_score}")
+    print(f"Best eps: {best_eps}")
+    print(f"Best min_samples: {best_min_samples}")
+    
+    return best_eps, best_min_samples
+"""
+
+if exists(path_artists):
     df_artists = pd.read_csv(path_artists)
-    df_artists['artist_genres'] = df_artists['artist_genres'].apply(ast.literal_eval)   # trasforma le concatenazioni dei generi musicali in una lista di stringhe dei generi
-    # df_artists = df_artists[df_artists['artist_genres'].apply(len) > 0]                 # elimina quegli artisti per cui non è stato definito il genere musicale
+    
+    # trasforma le concatenazioni dei generi musicali in una lista di stringhe dei generi
+    df_artists['artist_genres'] = df_artists['artist_genres'].apply(ast.literal_eval)   
       
     # ogni categoria viene smistata in una macro categoria
     for index, row in df_artists.iterrows():
@@ -80,11 +131,13 @@ if exists(path_saved_tracks) and exists(path_favorites_tracks) and exists(path_a
     
         df_artists.at[index, 'artist_genres'] = macro_genres
     
+    # trasformazione dei generi in colonne booleane
     mlb = MultiLabelBinarizer()
     genre_encoded = mlb.fit_transform(df_artists['artist_genres'])
     genre_df = pd.DataFrame(genre_encoded, columns=mlb.classes_)
     df_encoded = pd.concat([df_artists.drop('artist_genres', axis=1), genre_df], axis=1)
     
+    # eliminazione degli artisti per cui non è stato definito un genere
     genre_columns = df_encoded.columns[5:]
     sum_of_genres = df_encoded[genre_columns].sum(axis=1)
     df_filtered = df_encoded[sum_of_genres > 0]
@@ -96,69 +149,15 @@ if exists(path_saved_tracks) and exists(path_favorites_tracks) and exists(path_a
         "artist_popularity"
     ]
     
-    df_filtered.to_csv("temp.csv")
-            
-    # best_k_clustering(dataframe=df_filtered.drop(columns=columns_to_drop), max_value=101)
-    
     columns = list(df_filtered.columns)
     for column in columns_to_drop:
         columns.remove(column)
-        
-    clustering(df_filtered, columns, k = 10)
-
-    """
-    columns_to_scale = [
-        'track_bpm',
-        'track_energy',
-        'track_danceability',
-        'track_happiness',
-        'track_loudness',
-        'track_acousticness',
-        'track_instrumentalness',
-        'track_liveness',
-        'track_speechiness'
-    ]
     
-    clustering_columns = [
-        # 'track_bpm',
-        # 'track_duration',
-        'track_key',
-        'track_mode',
-        'track_time_signature'
-        
-        # 'track_energy',
-        # 'track_danceability',
-        # 'track_happiness',
-        # 'track_loudness',
-        # 'track_acousticness',
-        # 'track_instrumentalness',
-        # 'track_liveness',
-        # 'track_speechiness',
-    ]
-
-    # print(df_tracks[columns_to_scale].describe())
-
-    scaler = StandardScaler()
-    scaled_array = scaler.fit_transform(df_tracks[columns_to_scale])
-    scaled_dataframe = pd.DataFrame(scaled_array, columns=columns_to_scale)
+    ricerca_iperparametri(df_filtered[columns])
     
-    plt.figure(figsize=(15, 6))
-    sns.heatmap(df_tracks[columns_to_scale].corr(), annot=True)
-    plt.savefig("./img/heatmap.png")
-    
-    plt.figure(figsize=(15, 6))
-    sns.boxplot(df_tracks[columns_to_scale], orient="h")
-    plt.savefig("./img/boxplot_before_scaling.png")
-    
-    plt.figure(figsize=(15, 6))
-    sns.boxplot(scaled_dataframe, orient="h")
-    plt.savefig("./img/boxplot_after_scaling.png")
-    
-    # print(scaled_dataframe.describe())
-    """
-    
-    # df_tracks[columns_to_scale] = scaled_array
-    
-    # best_k_clustering(df_tracks[clustering_columns])
-    
-else: print("Non è stato possibile trovare il file descritto, ricontrolla il path")
+    # eps, min_samples = best_parameters_clustering(data=df_filtered[columns])
+      
+    dataframe = clustering(df_filtered[columns])
+    data = pd.concat([df_filtered.drop(columns, axis=1), dataframe], axis=1)  
+    data.to_csv("./datasets/clusteringDBSCAN.csv")
+else: print("Non è stato possibile trovare il file inserito, ricontrolla il path")
